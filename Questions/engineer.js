4fa09fbcaa9cddd369f4f0976b99f77f70aa5f65
Lucre <josielindamaldonado@gmail.com>
2021-04-13 23:53:39 -0400

    Validations were added to questions.

--- Questions/engineer.js
+++ Questions/engineer.js
@@ -3,21 +3,41 @@ const engineerQs = [
         type: "input",
         name: "engName",
         message: "Engineer name?",
+        validate: answer => {
+            if (answer !== '') {
+                return true
+            } return "Engineer name is required!"
+        }
     },
     {
         type: "input",
         name: "engId",
         message: "Engineer ID?",
+        validate: answer => {
+            if (answer !== '') {
+                return true
+            } return "Engineer ID is required!"
+        }
     },
     {
         type: "input",
         name: "engEmail",
         message: "Engineer Email?",
+        validate: answer => {
+            if (answer !== '') {
+                return true
+            } return "Engineer Email is required!"
+        }
     },
     {
         type: "input",
         name: "engGithub",
         message: "Engineer GitHub?",
+        validate: answer => {
+            if (answer !== '') {
+                return true
+            } return "Engineer Github is required!"
+        }
     },
 ]
 
--- Questions/intern.js
+++ Questions/intern.js
@@ -3,21 +3,41 @@ const internQs = [
         type: "input",
         name: "inName",
         message: "Intern name?",
+        validate: answer => {
+            if (answer !== '') {
+                return true
+            } return "Intern name is required!"
+        }
     },
     {
         type: "input",
         name: "inId",
         message: "Intern ID?",
+        validate: answer => {
+            if (answer !== '') {
+                return true
+            } return "Intern ID is required!"
+        }
     },
     {
         type: "input",
         name: "inEmail",
         message: "Intern Email?",
+        validate: answer => {
+            if (answer !== '') {
+                return true
+            } return "Intern Email is required!"
+        }
     },
     {
         type: "input",
         name: "inSchool",
         message: "Intern School?",
+        validate: answer => {
+            if (answer !== '') {
+                return true
+            } return "Intern School is required!"
+        }
     },
 ]
 
